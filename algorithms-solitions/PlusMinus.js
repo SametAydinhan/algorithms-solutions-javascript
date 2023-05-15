11710bb45a598a0f8d1f8573c64a91148930598a
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-05-15 21:38:58 +0300

    feat:add plus minus

--- algorithms-solitions/PlusMinus.js
+++ algorithms-solitions/PlusMinus.js
@@ -1,19 +1,19 @@
 function plusMinus(arr) {
     let n = arr.length;
     let poz = 0;
-    let neg = 0;
+    let nega = 0;
     let zer = 0;
     for(let a = 0; a< n;a++){
         if(arr[a]>0){
             poz++;
         }else if(arr[a]<0){
-            neg++;
+            nega++;
         }else{
             zer++;
         }
     }
     let pozkes = poz/n;
-    let negkes = neg/n;
+    let negkes = nega/n;
     let zerkes = zer/n;
     console.log(pozkes);
     console.log(negkes);
